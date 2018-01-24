0ad0c827e263ac6abccdd998948951d4621dc77e
alvivar <andresalvivar@gmail.com>
2018-01-24 16:42:34 -0600

    Comments + Lint conventions

--- chancho.py
+++ chancho.py
@@ -1,6 +1,6 @@
 """
-CHANCHO is a 4chan image downloader
-that keeps watching threads for new changes
+    CHANCHO is a 4chan image downloader
+    that keeps watching threads for new changes
 """
 
 import argparse
@@ -10,32 +10,41 @@ import sys
 import threading
 import time
 import urllib
-from multiprocessing import Queue
 
 import requests
-from lxml import _elementpath, html
+from lxml import html
 
 
 def download_4chan_thread(thread_url, path):
+    """
+        Downloads all images from a thread. Return a tuple with 2 lists, one of
+        downloaded urls, the other of previously downloaded.
+    """
     thread_name = thread_url.split("/")[-1]
     download_dir = os.path.join(path, "downloads", thread_name)
 
     images = get_4chan_images(thread_url)
-    down_now, down_previously = download_urls(images, download_dir)
-
+    now, previously = download_urls(images, download_dir)
     print(f"Thread {thread_name} complete!")
-    return down_now, down_previously
+
+    return now, previously
 
 
 def get_4chan_images(thread_url):
+    """
+        Return a list with all the images urls from a thread.
+    """
     page = requests.get(thread_url)
     tree = html.fromstring(page.content)
     pics = tree.xpath("//div[contains(@class, 'fileText')]/a/@href")
     return ["http://" + i[2:] for i in pics]
 
 
-def get_top_threads_from_board(board):
-    """ Get all the top threads from a board, ignoring sticky posts. """
+def get_threads_from_board(board):
+    """
+        Return a list with all the url threads from a board, ignoring sticky
+        posts.
+    """
     board_url = f"http://boards.4chan.org/{board}"
     page = requests.get(board_url)
     tree = html.fromstring(page.content)
@@ -46,8 +55,10 @@ def get_top_threads_from_board(board):
 
 
 def download_urls(urls, download_dir=""):
-    """ Returns a tuple with the last downloaded urls, and all the previously
-    downloaded urls. """
+    """
+        Downloads files from a list of urls. Return a tuple with 2 lists, one
+        of downloaded urls, the other of previously downloaded.
+    """
 
     # Create the dir
     if download_dir and not os.path.exists(download_dir):
@@ -91,104 +102,106 @@ if __name__ == "__main__":
     e'^_ )
       " " """)
 
-    # Command line args
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
+    # Command line
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument(
         "-t",
         "--threads",
         help="add threads to the download list",
         nargs="+",
         default=[])
-    parser.add_argument(
+    PARSER.add_argument(
         "-b",
         "--boards",
         help="add the top thread from each boards to the download list",
         nargs="+",
         default=[])
-    parser.add_argument(
+    PARSER.add_argument(
         "-w",
         "--wait",
         help="seconds to wait between full downloads, 60 seconds default",
         default=60,
         type=int)
-    parser.add_argument(
+    PARSER.add_argument(
         "-p",
         "--prune",
         help="archive old threads, 3600 seconds default (1h)",
         nargs="?",
         type=int,
         const=3600)
-    parser.add_argument("--bot", help="Feed me urls", action="store_true")
-    args = parser.parse_args()
+    PARSER.add_argument("--bot", help="Feed me threads", action="store_true")
+    ARGS = PARSER.parse_args()
 
     # frozen / not frozen, cxfreeze compatibility
-    current_dir = os.path.normpath(
+    HOMEDIR = os.path.normpath(
         os.path.dirname(
             sys.executable if getattr(sys, 'frozen', False) else __file__))
 
     # Files
-    threads_file = os.path.join(current_dir, "chanlist.json")
-    prune_file = os.path.join(current_dir, "pruned.json")
+    THREAD_FILE = os.path.join(HOMEDIR, "chanlist.json")
+    PRUNE_FILE = os.path.join(HOMEDIR, "pruned.json")
 
     # BOT mode
-    bot_urls = []
+    BOT_URLS = []
 
-    if args.bot:
+    if ARGS.bot:
         print('Feed me threads:')
 
         def BOT():
+            """
+                Handles the user input.
+            """
             while True:
                 text = input()
-                global bot_urls
-                bot_urls += [text]
+                global BOT_URLS
+                BOT_URLS += [text]
 
-        thread = threading.Thread(target=BOT)
-        thread.daemon = True
-        thread.start()
+        THREAD = threading.Thread(target=BOT)
+        THREAD.daemon = True
+        THREAD.start()
 
     # Downloads
     while True:
 
         # Read the queue
-        download_list = {}
-        if os.path.isfile(threads_file):
-            with open(threads_file) as f:
-                download_list = json.load(f)
+        DOWNLOAD_LIST = {}
+        if os.path.isfile(THREAD_FILE):
+            with open(THREAD_FILE) as f:
+                DOWNLOAD_LIST = json.load(f)
 
         # --boards Top threads
-        top_threads = []
-        if (args.boards):
-            top_threads = [
+        TOP_THREADS = []
+        if (ARGS.boards):
+            TOP_THREADS = [
                 thread
-                for board in args.boards
-                for thread in get_top_threads_from_board(board)[:1]
+                for board in ARGS.boards
+                for thread in get_threads_from_board(board)[:1]
             ]
 
-            if top_threads:
-                print(f"Top thread found: {top_threads[0]}")
+            if TOP_THREADS:
+                print(f"Top thread found: {TOP_THREADS[0]}")
 
         # Add the --threads, the input from the bot and the top threads from --boards
-        for u in args.threads + bot_urls + top_threads:
+        for u in ARGS.threads + BOT_URLS + TOP_THREADS:
             # TODO validate url
-            download_list[u] = download_list.get(u, {})
-        bot_urls = []
+            DOWNLOAD_LIST[u] = DOWNLOAD_LIST.get(u, {})
+        BOT_URLS = []
 
         # Exit on empty
-        if not download_list:
+        if not DOWNLOAD_LIST:
 
             # --bot
-            if args.bot:
+            if ARGS.bot:
                 continue
 
-            parser.print_usage()
+            PARSER.print_usage()
             print("the download list is empty, try '-u' to add urls")
-            parser.exit()
+            PARSER.exit()
 
         # Download everything, update statistics
         else:
-            for k, v in download_list.items():
-                down_now, down_previously = download_4chan_thread(
-                    k, current_dir)
+            for k, v in DOWNLOAD_LIST.items():
+                down_now, down_previously = download_4chan_thread(k, HOMEDIR)
                 v['images'] = len(down_now) + len(down_previously)
                 if down_now:
                     v['found'] = time.time()
@@ -202,46 +215,46 @@ if __name__ == "__main__":
                     v['prune'] = prune if prune > 0 else 0
 
                 # Save
-                with open(threads_file, "w") as f:
-                    json.dump(download_list, f)
+                with open(THREAD_FILE, "w") as f:
+                    json.dump(DOWNLOAD_LIST, f)
 
         # --prune
-        if args.prune:
+        if ARGS.prune:
 
             # Read archive
-            prune_list = {}
-            if os.path.isfile(prune_file):
-                with open(prune_file) as f:
-                    prune_list = json.load(f)
+            PRUNE_LIST = {}
+            if os.path.isfile(PRUNE_FILE):
+                with open(PRUNE_FILE) as f:
+                    PRUNE_LIST = json.load(f)
 
             # Clean up current, archive prune
-            clean_download_list = {}
-            for k, v in download_list.items():
-                if v.get('prune', 0) > args.prune:
-                    prune_list[k] = v
+            CLEAN_DOWNLOAD_LIST = {}
+            for k, v in DOWNLOAD_LIST.items():
+                if v.get('prune', 0) > ARGS.prune:
+                    PRUNE_LIST[k] = v
                 else:
-                    clean_download_list[k] = v
-            prune_count = len(download_list) - len(clean_download_list)
-            download_list = clean_download_list
+                    CLEAN_DOWNLOAD_LIST[k] = v
+            PRUNE_COUNT = len(DOWNLOAD_LIST) - len(CLEAN_DOWNLOAD_LIST)
+            DOWNLOAD_LIST = CLEAN_DOWNLOAD_LIST
 
-            if prune_count > 0:
-                print(f"{prune_count} thread{'s' if prune_count > 1 else ''} "
-                      f"pruned ({args.prune}s old)")
+            if PRUNE_COUNT > 0:
+                print(f"{PRUNE_COUNT} thread{'s' if PRUNE_COUNT > 1 else ''} "
+                      f"pruned ({ARGS.prune}s old)")
 
             # Save
-            with open(prune_file, "w") as f:
-                json.dump(prune_list, f)
+            with open(PRUNE_FILE, "w") as f:
+                json.dump(PRUNE_LIST, f)
 
-            with open(threads_file, "w") as f:
-                json.dump(download_list, f)
+            with open(THREAD_FILE, "w") as f:
+                json.dump(DOWNLOAD_LIST, f)
 
             # Nothing else to do
-            if len(download_list) < 1:
+            if len(DOWNLOAD_LIST) < 1:
                 print()
                 continue
 
         # --rest between complete downloads
-        print(f"Waiting {args.wait}s to retry")
-        if args.bot:
-            print("Feed me urls:")
-        time.sleep(args.wait)
+        print(f"Waiting {ARGS.wait}s to retry")
+        if ARGS.bot:
+            print("Feed me threads:")
+        time.sleep(ARGS.wait)
