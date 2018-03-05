10265d21ca714a275bf457420d4946737ba4bc34
alvivar <andresalvivar@gmail.com>
2018-03-05 02:22:44 -0600

    No commands | Auto add pasted urls | Errors removed | repeat isn't default

--- chancho.py
+++ chancho.py
@@ -9,7 +9,9 @@ import os
 import sys
 import threading
 import time
-import urllib
+from urllib.parse import urlparse
+from urllib.request import urlopen
+from urllib.error import HTTPError
 
 import requests
 from lxml import html
@@ -29,9 +31,9 @@ def download_4chan_thread(threadurl, path):
     now, previously = download_urls(images, download_dir)
 
     if now or previously:
-        print(f"Thread {thread_name} complete!")
+        print(f"thread {thread_name} complete!")
     else:
-        print(f"Invalid thread (no images): {threadurl}")
+        print(f"invalid thread (no images): {threadurl}")
 
     return now, previously
 
@@ -46,7 +48,7 @@ def get_4chan_images(threadurl):
         tree = html.fromstring(page.content)
         pics = tree.xpath("//div[contains(@class, 'fileText')]/a/@href")
     except requests.exceptions.MissingSchema:
-        print(f"Invalid url: {threadurl}")
+        print(f"invalid url: {threadurl}")
         return []
 
     return ["http://" + i[2:] for i in pics]
@@ -94,20 +96,20 @@ def download_urls(urls, download_dir=""):
     for i, url in enumerate(urls):
         name = url.split("/")[-1]
         filename = os.path.join(download_dir, name)
-        print(f"Downloading {i+1}/{len(urls)} {url}")
+        print(f"downloading {i+1}/{len(urls)} {url}")
 
         try:
-            with urllib.request.urlopen(url) as r, open(filename, 'wb') as f:
+            with urlopen(url) as r, open(filename, 'wb') as f:
                 data = r.read()
                 f.write(data)
             downloaded.append(url)
 
             # Save
-            with open(before_file, "w") as f:
+            with open(before_file, 'w') as f:
                 json.dump(downloaded + downloaded_before, f)
 
-        except urllib.error.HTTPError:
-            print(f"Error downloading {url}")
+        except HTTPError:
+            print(f"error downloading {url}")
 
     return downloaded, downloaded_before
 
@@ -125,8 +127,6 @@ if __name__ == "__main__":
         description=
         "4chan image downloader that keeps watching threads for new changes")
     PARSER.add_argument(
-        "-s", "--start", help="start the cycle", action="store_true")
-    PARSER.add_argument(
         "-t",
         "--threads",
         help="add threads urls to the download list",
@@ -140,10 +140,11 @@ if __name__ == "__main__":
         nargs="+",
         default=[])
     PARSER.add_argument(
-        "-w",
-        "--wait",
-        help="seconds to wait between full downloads, 60 seconds default",
-        default=60,
+        "-r",
+        "--repeat",
+        help=
+        "seconds to wait before repeating the cycle again, 60 seconds default",
+        default=0,
         type=int)
     PARSER.add_argument(
         "-p",
@@ -153,12 +154,6 @@ if __name__ == "__main__":
         type=int,
         const=3600)
     ARGS = PARSER.parse_args()
-
-    # --start is required
-    if not ARGS.start:
-        print("try -s to start the cycle")
-        PARSER.print_usage()
-        PARSER.exit()
 
     # frozen / not frozen, cxfreeze compatibility
     HOMEDIR = os.path.normpath(
@@ -183,53 +178,31 @@ if __name__ == "__main__":
 
             if len(commands) < 1:
                 return
-            commands[0] = commands[0].replace('-', '')
 
             # Quit
-            if commands[0] == 'q':
-                print(CHANCHO + " Bye!")
+            if commands[0] in ['q']:
+                print(CHANCHO + " bye!")
                 time.sleep(1)
 
                 global REPEAT
                 REPEAT = False
                 sys.exit(0)
 
-            # Start
-            elif commands[0] in ['s', 'start']:
-                print("chancho: i'm already running!")
-
-            # Help
-            elif commands[0] in ['h', 'help', 'command', 'commands']:
-                PARSER.print_help()
-
-            # Threads
-            elif commands[0] in ['t', 'thread', 'threads']:
-                ARGS.threads += commands[1:] if len(commands) > 1 else []
-                print(
-                    f"chancho: adding threads urls: {' '.join(commands[1:])}")
-
-            # Boards
-            elif commands[0] in ['b', 'board', 'boards']:
-                ARGS.boards += commands[1:] if len(commands) > 1 else []
-                print(f"chancho: adding boards: {', '.join(commands[1:])}")
-
-            # Wait
-            elif commands[0] in ['w', 'wait']:
-                ARGS.wait = cutetime.toseconds(
-                    commands[1]) if len(commands) > 1 else 60
-                print(f"chancho: wait time is {ARGS.wait}s now")
-
-            # Prune
-            elif commands[0] in ['p', 'prune']:
-                ARGS.prune = cutetime.toseconds(
-                    commands[1]) if len(commands) > 1 else 3600
-                print(f"chancho: prune time is {ARGS.prune}s now")
-
             else:
-                print(f"chancho: i don't understand '{commands[0]}', try")
-                PARSER.print_usage()
+                threads = [i for i in commands if urlparse(i).scheme]
+                ARGS.threads += threads
 
-            print()
+                boards = []
+                for word in commands:
+                    board = ''.join(letter for letter in word)
+                    if len(board) < 5:
+                        boards.append(board)
+                ARGS.boards += boards
+
+                if threads:
+                    print(f"new threads: {', '.join(threads)}")
+                if boards:
+                    print(f"new boards for top scan: {', '.join(boards)}")
 
     THREAD = threading.Thread(target=botmode)
     THREAD.daemon = True
@@ -253,7 +226,7 @@ if __name__ == "__main__":
             ]
 
             if TOP_THREADS:
-                print(f"Top thread found: {TOP_THREADS[0]}")
+                print(f"top thread found: {TOP_THREADS[0]}")
 
         # Add the --threads, the input from the bot and the top threads from --boards
         for u in ARGS.threads + TOP_THREADS:
@@ -262,8 +235,10 @@ if __name__ == "__main__":
 
         # Download everything, update statistics
         for k, v in DOWNLOAD_LIST.items():
+
             down_now, down_previously = download_4chan_thread(k, HOMEDIR)
-            v['images'] = len(down_now) + len(down_previously)
+            image_count = len(down_now) + len(down_previously)
+            v['images'] = image_count
             if down_now:
                 v['found'] = time.time()
                 v['prune'] = 0
@@ -275,9 +250,24 @@ if __name__ == "__main__":
                 prune = time.time() - v['found']
                 v['prune'] = prune if prune > 0 else 0
 
+            # It's an error, prune it
+            if image_count < 1:
+                DOWNLOAD_LIST[k]['error'] = True
+
             # Save
-            with open(THREAD_FILE, "w") as f:
+            with open(THREAD_FILE, 'w') as f:
                 json.dump(DOWNLOAD_LIST, f)
+
+        # Remove errors
+        DOWNLOAD_LIST = {
+            k: v
+            for k, v in DOWNLOAD_LIST.items()
+            if not DOWNLOAD_LIST[k].get('error', False)
+        }
+
+        # Save
+        with open(THREAD_FILE, 'w') as f:
+            json.dump(DOWNLOAD_LIST, f)
 
         # --prune
         if ARGS.prune:
@@ -291,7 +281,7 @@ if __name__ == "__main__":
             # Clean up current, archive prune
             CLEAN_DOWNLOAD_LIST = {}
             for k, v in DOWNLOAD_LIST.items():
-                if v.get('prune', 0) > ARGS.prune:
+                if v.get('prune', 0) >= ARGS.prune:
                     PRUNE_LIST[k] = v
                 else:
                     CLEAN_DOWNLOAD_LIST[k] = v
@@ -303,22 +293,29 @@ if __name__ == "__main__":
                       f"pruned ({ARGS.prune}s old)")
 
             # Save
-            with open(PRUNE_FILE, "w") as f:
+            with open(PRUNE_FILE, 'w') as f:
                 json.dump(PRUNE_LIST, f)
 
-            with open(THREAD_FILE, "w") as f:
+            with open(THREAD_FILE, 'w') as f:
                 json.dump(DOWNLOAD_LIST, f)
 
-            # Nothing else to do
-            if len(DOWNLOAD_LIST) < 1:
+        # Nothing else to do
+        if len(DOWNLOAD_LIST) < 1:
+            print()
+            PARSER.print_usage()
+            PARSER.exit()
+
+        # --repeat between complete downloads
+        if ARGS.repeat:
+            print(f"\nwaiting {ARGS.repeat} seconds to retry...")
+            print("paste threads, boards or 'q' + enter to quit:")
+
+            WAIT = 0
+            while REPEAT and WAIT < ARGS.repeat:
+                WAIT += 1
+                time.sleep(1)
+            if REPEAT:
                 print()
-                continue
 
-        # --rest between complete downloads
-        print(f"\nWaiting {ARGS.wait} seconds to retry...")
-        print("Feed me commands | 'q' to quit: \n")
-
-        WAIT = 0
-        while REPEAT and WAIT < ARGS.wait:
-            WAIT += 1
-            time.sleep(1)
+        else:  # do while
+            REPEAT = False
